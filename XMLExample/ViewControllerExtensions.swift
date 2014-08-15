e74e96bc6e78bbee8e0a4fb485cc0e8d67c123dc
Stuart Breckenridge <stuart@darwin.io>
2014-08-15 23:30:32 +0800

    - Created separate Extensions file for the ViewController.

---
+++ XMLExample/ViewControllerExtensions.swift
@@ -0,0 +1,50 @@
+//
+//  ViewControllerExtensions.swift
+//  XMLExample
+//
+//  Created by Stuart Breckenridge on 15/8/14.
+//  Copyright (c) 2014 Stuart Breckenridge. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension ViewController: UITableViewDataSource
+{
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int
+    {
+        return self.theApps.count
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!
+    {
+        let cellIdentifier = "Cell"
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as AppCell
+        
+        var app = theApps[indexPath.row] as App
+        
+        cell.appName.text = app.appName
+        cell.appPrice.text = app.appPrice
+        cell.appRights.text = app.appRights
+        cell.appSummary.text = app.appSummary
+        
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate
+{
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+}
+
+extension ViewController: XMLParserNotifications
+{
+    func didFinishParsingApps(apps:[App])
+    {
+        self.theApps = apps
+        self.appTableView.reloadData()
+    }
+}
